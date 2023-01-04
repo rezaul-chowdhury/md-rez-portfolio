b14efbda8d61115ca4953da9faf05c25ec517371
rezaul choudhury <rofy2009@gmail.com>
2023-01-04 13:05:17 +0000

    edit the layout

--- src/components/elements/Cards.jsx
+++ src/components/elements/Cards.jsx
@@ -39,7 +39,7 @@ const cards = [
     },
     {
         id: 5,
-        img: require("../../images/rplumbing.jpg.jpg"),
+        img: require("../../images/r-plumbing.jpg"),
         title: 'R-PLUMING',
         details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         button: <a href="https://rplumbing.netlify.app/" target="_blank" rel="noopener">
